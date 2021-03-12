92921854747008572f52f83c0c1533481f5bc544
ocnelias <ocnelias@gmail.com>
2021-03-12 12:57:52 +0200

    fix: fix function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+
+module.exports = function towelSort(matrix) {
+    if (matrix !== undefined) {
+        let res = [];
+
+        for (let i = 0; i <= matrix.length - 1; i++) {
+            if (i % 2 !== 0) {
+                for (let k = matrix[i].length - 1; k >= 0; k--) {
+                    res.push(matrix[i][k]);
+                }
+            } else {
+                for (let k = 0; k <= matrix[i].length - 1; k++) {
+                    res.push(matrix[i][k]);
+                }
+            }
+        }
+        return res;
+    }
+    return [];
 }
+
+
+
